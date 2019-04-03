16c29632600709c1da73eeff6adf279ce1bd3c7e
DJEBROUNI Khaled <48313244+khaledDjebrouni@users.noreply.github.com>
2019-04-03 15:59:56 +0100

    Add files via upload
    j'ai réécrit la fonction parcours en largeur+createformule avec les fichiers+un interface pour faciliter l'exploitation

---
+++ All.java
@@ -0,0 +1,70 @@
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class All 
+{
+	//==========================================
+	//	
+	//==========================================
+	public static void main(String[] args) throws IOException 
+	{
+		CNF cnf	=	new	CNF();
+		
+		cnf.createformule();
+		int		leChoix;
+		
+		@SuppressWarnings("resource")
+		Scanner s	=	new Scanner(System.in);
+		
+		do
+		{
+			
+			System.out.printf("\n\n\t\tvoilà les options qu'il existe:\n");
+			System.out.println("0-arrêter le programme:");
+			System.out.println("1-afficher la formule:");
+			System.out.println("2-parcours en largeur:");
+			System.out.println("3-parcours en profondeur:");
+			System.out.println("4-génération des solutions aléatoire:");
+			
+			System.out.println("quel est votre choix ?");
+			leChoix		=	s.nextInt();
+			
+			
+			
+			switch(leChoix)
+			{
+				case 0:	
+					System.out.println("au revoir !");
+					System.exit(0);
+					break;
+				case 1:	
+					cnf.showFormule();
+					break;
+						
+				case 2:	
+					System.out.println("parcour par largeur :");
+					cnf.rechLargeur();
+					break;
+				
+				case 3:	
+					System.out.println("parcour par profondeur :");
+					cnf.rechProfondeur(new ArrayList<Integer>());
+					break;
+				
+				case 4:
+					System.out.println("recherche Aléatoire :");
+					cnf.rechercheAléatoire();
+					break;
+				default:
+					System.out.println("ce choix n'existe plus");
+			}
+		}while(true);	
+		
+		
+		
+		
+	}
+	
+	
+}
